e7a3cf4db364e502a9cd6803cb82faf5740fab7e
Farai Wande <faraiwande@outlook.com>
2024-10-31 14:41:54 +0000

    try block

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -28,16 +28,17 @@ def process_orders(app):
             "customer": order.customer,
             "date": order.date_placed_local.isoformat(),
         }
-
-        response = requests.post(
-            app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
-            json=payload
-        )
-        app.logger.info("Response from endpoint: " + response.text)
-        response.raise_for_status()
-
-        order.set_as_processed()
-        save_order(order)
+        try:
+            response = requests.post(
+                app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
+                json=payload
+            )
+            response.raise_for_status()
+            order.set_as_processed()
+            save_order(order)
+        except:
+            app.logger.info("Response from endpoint: " + response.text)
+        
 
 def get_queue_of_orders_to_process():
     allOrders = get_all_orders()
